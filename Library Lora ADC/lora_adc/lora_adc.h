b80ec1f4a989a80c4a812fa29b7c8f51ed0d0fa5
Jimmi Kardo Sitepu <32915206+jimmisitepu88@users.noreply.github.com>
2020-03-08 15:55:09 +0700

    Update lora_adc.h

--- Library Lora ADC/lora_adc/lora_adc.h
+++ Library Lora ADC/lora_adc/lora_adc.h
@@ -7,8 +7,8 @@ class lora_adc
 private:
     byte RST; unsigned int BAUD;
 public:
-    lora_adc(byte pinRST, unsigned int baud);
-    void begin(byte pinRST, unsigned int baud);
+    //lora_adc(byte pinRST, unsigned int baud);
+    void begin();
     bool getADC();
     int adc0();int adc1();int adc2();int adc3();
     int adc4();int adc5();int adc6();int adc7();
